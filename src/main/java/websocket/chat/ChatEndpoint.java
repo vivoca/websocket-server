5251bba9ee2fb81353d11a7b9d021ab850032935
egyud_v <vivien.egyud@gmail.com>
2017-03-13 15:26:43 +0100

    js websocket chat for demo

---
+++ src/main/java/websocket/chat/ChatEndpoint.java
@@ -0,0 +1,18 @@
+package websocket.chat;
+
+import java.io.IOException;
+import javax.websocket.EncodeException;
+import javax.websocket.OnMessage;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/chat")
+public class ChatEndpoint {
+    @OnMessage
+    public void message(String message, Session client) throws IOException, EncodeException {
+        System.out.println("message: " + message);
+        for (Session peer : client.getOpenSessions()) {
+            peer.getBasicRemote().sendText(message);
+        }
+    }
+}
